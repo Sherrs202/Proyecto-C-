5595ac1dbd47ac8ffb517711ff3249345f580154
Sherrs202 <A01714794@tec.mx>
2025-12-03 00:26:30 -0600

    Refactor main.cpp to use classes and menus

--- main.cpp
+++ main.cpp
@@ -1,22 +1,85 @@
 #include <iostream>
+#include <vector>
+#include <limits>
+#include "Bebida.h"
+#include "Pedido.h"
+#include "Propina.h"
+
 using namespace std;
 
-// Incluyendo los .cpp directamente
-#include "Bebidas.cpp"
-#include "Pedido.cpp"
-#include "Propina.cpp"
-
 int main() {
-    Bebida b1("Centenario Plata", "Tequila", 90);
+    vector<Pedido> listaPedidos;
+    int opcion;
 
-    Pedido p1(2, b1.getPrecio());
+    do {
+        cout << "\n===== MENU =====\n";
+        cout << "1. Registrar bebida \n";
+        cout << "2. Ver pedidos y pagar\n";
+        cout << "3. Salir\n";
+        cout << "Elige una opcion: ";
+        cin >> opcion;
 
-    int subtotal = p1.calcularSubtotal();
-    cout << "Subtotal: " << subtotal << endl;
+        if (opcion == 1) {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
 
-    Propina prop;
-    double propinaTotal = prop.calcular(10, subtotal);
-    cout << "Propina: " << propinaTotal << endl;
+            string nombre, tipo;
+            double precio;
+            int cantidad;
+
+            cout << "\nNombre de la bebida: ";
+            getline(cin, nombre);
+
+            cout << "Tipo (ej: Tequila, Cerveza, Refresco): ";
+            getline(cin, tipo);
+
+            cout << "Precio: $";
+            cin >> precio;
+
+            cout << "Cantidad: ";
+            cin >> cantidad;
+
+            
+            Bebida b(nombre, precio, tipo);
+            Pedido p(b, cantidad);
+
+            listaPedidos.push_back(p);
+
+            cout << "\nBebida registrada con exito!\n";
+        }
+        else if (opcion == 2) {
+            if (listaPedidos.empty()) {
+                cout << "\nNo hay pedidos registrados.\n";
+                continue;
+            }
+
+            cout << "\n=== Pedidos Registrados ===\n";
+            double subtotalTotal = 0.0;
+
+            for (size_t i = 0; i < listaPedidos.size(); ++i) {
+                cout << "\nPedido #" << i + 1 << endl;
+                listaPedidos[i].mostrarPedido();
+                subtotalTotal += listaPedidos[i].calcularSubtotal();
+            }
+
+            cout << "\nSubtotal total: $" << subtotalTotal << endl;
+
+            double porcentaje;
+            cout << "¿Cuánto % de propina quieres dejar?: ";
+            cin >> porcentaje;
+
+            double propina = Propina::calcular(porcentaje, subtotalTotal);
+
+            cout << "\nPropina: $" << propina << endl;
+            cout << "Total a pagar: $" << (subtotalTotal + propina) << endl;
+        }
+        else if (opcion == 3) {
+            cout << "Saliendo...\n";
+        }
+        else {
+            cout << "Opcion invalida.\n";
+        }
+
+    } while (opcion != 3);
 
     return 0;
 }
